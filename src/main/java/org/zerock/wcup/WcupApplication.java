c3078d91daebeca41a56e0209ca22f021e38316c
zk202308a@gmail.com <zk202308a@gmail.com>
2024-01-09 10:09:46 +0900

    Initial commit

---
+++ src/main/java/org/zerock/wcup/WcupApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.wcup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WcupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zerock/wcup/WcupApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.wcup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WcupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
