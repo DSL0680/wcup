f39e44a93cd71c597825b20d5f9c2e4e58fcaa1e
zk202308a@gmail.com <zk202308a@gmail.com>
2024-01-10 13:53:01 +0900

    DB테스트

--- src/test/java/org/zerock/wcup/WcupApplicationTests.java
+++ src/test/java/org/zerock/wcup/WcupApplicationTests.java
@@ -1,10 +1,34 @@
 package org.zerock.wcup;
 
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.sql.DataSource;
+
 @SpringBootTest
+@Log4j2
 class WcupApplicationTests {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Test
+    public void testConnection() throws Exception {
+
+        String sql = "select now()";
+
+        try (var con = dataSource.getConnection();
+             var pstmt = con.prepareStatement(sql);
+             var rs = pstmt.executeQuery()) {
+
+            if (rs.next()) {
+                log.info(rs.getString(1));
+            }
+        }//end tr
+
+    }
 
     @Test
     void contextLoads() {
