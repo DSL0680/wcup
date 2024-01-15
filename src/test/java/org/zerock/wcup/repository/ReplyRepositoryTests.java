ce40703a5bf0437a8a962ce3f63870a74ad66fd8
zk202308a@gmail.com <zk202308a@gmail.com>
2024-01-15 13:20:21 +0900

    JPA4-2

--- src/test/java/org/zerock/wcup/repository/ReplyRepositoryTests.java
+++ src/test/java/org/zerock/wcup/repository/ReplyRepositoryTests.java
@@ -1,10 +1,15 @@
 package org.zerock.wcup.repository;
 
 
+import jakarta.transaction.Transactional;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.wcup.domain.Board;
+import org.zerock.wcup.domain.Reply;
+
+import java.util.Optional;
 
 @SpringBootTest
 @Log4j2
@@ -17,7 +22,30 @@ public class ReplyRepositoryTests {
     public void testInsert() {
         //111, 109, 108, 106
 
+        Long bno = 106L;
+        for (int i = 0; i < 5; i++) {
+            Board board = Board.builder().bno(bno).build();
 
+            Reply reply = Reply.builder()
+                    .replyText("Test")
+                    .replyer("r1")
+                    .board(board)
+                    .build();
+
+            log.info(replyRepository.save(reply));
+        }//end for
     }
+
+
+//    @Test
+//    public void testRead(){
+//        Long rno = 20L;
+//
+//        Optional<Reply> result = replyRepository.findById(rno);
+//
+//        Reply reply = result.orElseThrow();
+//
+//        log.info(reply);
+//    }
 
 }
